cef3b16c03970d620b2154d23de7b4f2feaf4bd8
Red_M <git@red-m.net>
2026-03-14 22:49:24 +1000

    Add service mode

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,17 @@ use config::Config;
 use std::sync::Arc;
 use tracing::{error, info};
 use xdg::BaseDirectories;
+use argh::FromArgs;
 
 const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const CONFIG_FILE: &str = "chaperone.toml";
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh( description = "xr-chaperone")]
+pub struct CliArgs {
+    #[argh(switch, short = 's', description = "run without a GUI for use in a systemd service, don't use this if you haven't configured xr-chaperone with the GUI at least once.")]
+    service_mode: bool,
+}
 
 /// Entry point, loads the config, starts the xr thread, spawns the interface
 fn main() -> Result<()> {
@@ -29,6 +37,7 @@ fn main() -> Result<()> {
         )
         .init();
 
+    let cli_args: CliArgs = argh::from_env();
     let xdg_dirs = BaseDirectories::with_prefix(APP_NAME);
     let cfg_path = xdg_dirs.place_config_file(CONFIG_FILE);
     if let Err(e) = cfg_path {
@@ -58,7 +67,7 @@ fn main() -> Result<()> {
     // Spawn XR render thread
     let xr_state = Arc::clone(&state);
     let xr_cfg = cfg.clone();
-    std::thread::Builder::new()
+    let xr_thread_handle = std::thread::Builder::new()
         .name("xr-render".into())
         .spawn(move || xr_thread::run_xr_thread(xr_state, xr_cfg))?;
 
@@ -82,7 +91,12 @@ fn main() -> Result<()> {
                 error!("Failed to Set Offsets: {}", e);
             }
 
-            ui::run(state, cfg, cfg_path)?;
+            if cli_args.service_mode == true {
+                info!("Requested service mode, not starting the GUI.");
+                let res = xr_thread_handle.join();
+            } else {
+                ui::run(state, cfg, cfg_path)?;
+            }
         }
     }
 
